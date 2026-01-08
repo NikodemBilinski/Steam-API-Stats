49a53aee80d99aa20d405887d9cb1a684fb1335d
Nikodemus <nikodembilinski4@gmail.com>
2026-01-08 17:49:07 +0100

    Update 1.4
    Added showing recent games, changed code to be more failsafe in situations for e.g. when someone has his friendlist private, but its not completed yet.
    
    I will add messages in html when something is private, to make it clear

--- asp.net steam api aplikacja/Pages/Index.cshtml.cs
+++ asp.net steam api aplikacja/Pages/Index.cshtml.cs
@@ -11,16 +11,21 @@ namespace asp.net_steam_api_aplikacja.Pages
         public string SteamID { get; set; }
 
         public string RawJsonSummaries { get; set; }
-
         public string RawJsonFriendList { get; set; }
+        public string RawJsonLastPlayed { get; set; }
+
+        public JsonElement RecentGamesArray { get; set; }
 
         public string UserName { get; set; }
-
         public string UserAvatar { get; set; }
-
         public string UserStatus { get; set; }
 
-        public int FriendCount { get; set; }
+        public int FriendCount;
+        public int LastGamesCount;
+        public bool Profile_Visible;
+        public bool Friendlist_Visible;
+        public bool RecentlyPlayed_Visible;
+        public bool Games_Visible;
 
 
         [BindProperty]
@@ -28,45 +33,100 @@ namespace asp.net_steam_api_aplikacja.Pages
 
         public async Task OnPostAsync()
         {
+            // url 1 - Player Summaries
             var url1 = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=" + apiKey + "&steamids=" + SteamID;
 
-            // "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key="+apiKey+"&steamids="+SteamID;
-            // https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=NOWY_KLUCZ&steamid=STEAMID&include_appinfo=1 For debugging purposes
-
-            Console.WriteLine(SteamID);
-            Console.WriteLine(apiKey);
             using var http = new HttpClient();
 
+            
             RawJsonSummaries = await http.GetStringAsync(url1);
+
 
             using var jsonDoc = JsonDocument.Parse(RawJsonSummaries);
 
-            var User = jsonDoc.RootElement.GetProperty("response").GetProperty("players")[0];
+            var response = jsonDoc.RootElement.GetProperty("response");
 
-            UserName = User.GetProperty("personaname").GetString();
+            if (response.TryGetProperty("players", out JsonElement User) && User.GetArrayLength() > 0)
+            {
+                User = jsonDoc.RootElement.GetProperty("response").GetProperty("players")[0];
 
-            UserAvatar = User.GetProperty("avatarfull").GetString();
+                UserName = User.GetProperty("personaname").GetString();
 
-            UserStatus = User.GetProperty("personastate").ToString();
+                UserAvatar = User.GetProperty("avatarfull").GetString();
+
+                UserStatus = User.GetProperty("personastate").ToString();
+
+            }
+            else
+            {
+                Profile_Visible = false;
+                Console.WriteLine("Profil prywatny");
+            }
+               
+            // url 2 - Friends List
 
             var url2 = "http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=" + apiKey + "&steamid=" + SteamID+"&relationship=friend";
 
-            using var http2 = new HttpClient();
+            
+            try
+            {
+                RawJsonFriendList = await http.GetStringAsync(url2);
 
-            RawJsonFriendList = await http2.GetStringAsync(url2);
+                using var JsonDoc2 = JsonDocument.Parse(RawJsonFriendList);
 
-            using var JsonDoc2 = JsonDocument.Parse(RawJsonFriendList);
+                if (JsonDoc2.RootElement.TryGetProperty("friendslist", out JsonElement FriendList) && FriendList.GetProperty("friends").GetArrayLength() > 0)
+                {
+                    FriendCount = FriendList.GetProperty("friends").GetArrayLength();
+                    Console.WriteLine("list jest");
+                    Friendlist_Visible = true;
+                }
+                else
+                {
+                    Friendlist_Visible = false;
+                    Console.WriteLine("Friends Private");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Error");
+            }
+            
 
-            var FriendList = JsonDoc2.RootElement.GetProperty("friendslist").GetProperty("friends");
+            // url 3 - Last Played
 
-            FriendCount = FriendList.GetArrayLength();
+            var url3 = "http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=" + apiKey + "&steamid=" + SteamID + "&format=json" + "&count=9";
 
 
+            RawJsonLastPlayed = await http.GetStringAsync(url3);
 
-            Console.WriteLine(FriendCount);
-           /* Console.WriteLine(UserName);
-            Console.WriteLine(UserAvatar);
-            Console.WriteLine(UserStatus);*/
+            var JsonDoc3 = JsonDocument.Parse(RawJsonLastPlayed);
+
+            //var LastPlayed = JsonDoc3.RootElement.GetProperty("response").GetProperty("games");
+
+            if(JsonDoc3.RootElement.TryGetProperty("response",out JsonElement test) && test.TryGetProperty("games",out JsonElement RecentGames))
+            {
+                RecentGamesArray = RecentGames;
+                LastGamesCount = RecentGames.GetArrayLength();
+                RecentlyPlayed_Visible = true;
+            }
+            else
+            {
+                RecentlyPlayed_Visible = false;
+                Console.WriteLine("recent games private");
+            }
+
+
+                // url 4 - Games
+
+                //DEBUGGING 
+
+            //Console.WriteLine(RawJsonFriendList);
+            //Console.WriteLine(RawJsonLastPlayed);
+            //Console.WriteLine("last games: "+ LastGamesCount);
+            //Console.WriteLine(FriendCount);
+            //Console.WriteLine(UserName);
+            //Console.WriteLine(UserAvatar);
+            //Console.WriteLine(UserStatus);
         }
     }
 }
